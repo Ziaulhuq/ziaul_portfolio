f602eebfa2aed0db9a65c2770474ce2c1708babc
ZIA UL HUQ <ziaulhuq@ZIAs-MacBook-Pro.local>
2025-11-03 09:49:22 +0800

    update loading component

---
+++ app/loading.tsx
@@ -0,0 +1,14 @@
+import { Loader } from "lucide-react";
+
+const loading = () => {
+  return (
+    <div className="flex flex-col h-screen justify-center items-center">
+      <div>
+        <Loader className="animate-spin w-16 h-16 text-blue-500" />
+      </div>
+      <div>loading...</div>
+    </div>
+  );
+};
+
+export default loading;
